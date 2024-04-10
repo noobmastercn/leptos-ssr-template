a3e403d2810b286eef8776662b754d6a8947b82e
lilpum <lilpum@qq.com>
2024-04-11 03:00:04 +0800

    chore: 构建过程或辅助工具的变动

--- src/app.rs
+++ src/app.rs
@@ -2,7 +2,6 @@ use crate::error_template::{AppError, ErrorTemplate};
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
-use tracing::info;
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -10,8 +9,6 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-
-
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/leptos-ssr-template.css"/>
@@ -43,7 +40,6 @@ fn HomePage() -> impl IntoView {
     // Creates a reactive value to update the button
     let (count, set_count) = create_signal(0);
     let on_click = move |_| set_count.update(|count| *count += 1);
-    info!("================HomePage rendered");
 
     view! {
         <div class="flex justify-center items-center h-screen">
